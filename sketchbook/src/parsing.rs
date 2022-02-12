9e1b1ad85f772fe103c5d9bff3a35831a7649d52
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-02-11 20:53:41 -0600

    Add new sketches

--- sketchbook/src/parsing.rs
+++ sketchbook/src/parsing.rs
@@ -3,7 +3,7 @@ use crate::error::{SketchbookError, SketchbookResult};
 use regex::Regex;
 
 pub(crate) fn try_sketch_name_from_str(arg: &str) -> SketchbookResult<String> {
-    let re = Regex::new(r"[^-_.a-zA-Z0-9]").unwrap();
+    let re = Regex::new(r"[^ -_.a-zA-Z0-9]").unwrap();
     if re.is_match(arg) {
         Err(SketchbookError::InvalidSketchName(arg.to_string()))
     } else {
--- sketchbook/src/sketch.rs
+++ sketchbook/src/sketch.rs
@@ -1,10 +1,20 @@
 #![allow(dead_code)]
 
-use sketches::templates::blank::main as blank_main;
+use sketches::{
+    sketches::{
+        basic_2d_main,
+        ten_print_main,
+    },
+    templates::{
+        blank_main,
+    },
+};
 
 #[derive(Debug)]
 pub enum Sketch {
+    Basic2d,
     Blank,
+    TenPrint,
 }
 
 pub type SketchEntrypoint = fn() -> ();
@@ -12,7 +22,9 @@ pub type SketchEntrypoint = fn() -> ();
 impl Sketch {
     pub fn to_entrypoint(&self) -> SketchEntrypoint {
         match self {
+            Sketch::Basic2d => basic_2d_main,
             Sketch::Blank => blank_main,
+            Sketch::TenPrint => ten_print_main,
         }
     }
 }
@@ -28,7 +40,9 @@ impl std::str::FromStr for Sketch {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
+            "basic_2d" | "Basic 2D" => Ok(Sketch::Basic2d),
             "blank" | "Blank" => Ok(Sketch::Blank),
+            "ten_print" | "Ten Print" => Ok(Sketch::TenPrint),
             _ => Err(format!("\"{}\" is not a valid sketch", s)),
         }
     }
